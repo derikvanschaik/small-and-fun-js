24602d39d4fe69f9dcb4216722156d0e27a0c708
derikvanschaik <derikvanschaik@gmail.com>
2021-08-31 23:38:24 -0700

    word search works well now, cursor issue resolved.

--- wordsearcher.js
+++ wordsearcher.js
@@ -1,28 +1,23 @@
-// NOT COMPLETED YET -- JUST THE FOUNDATION FOR A WORDSEARCHER
-// PROGRAM WHERE USER PUTS IN THEIR WORD AND PROGRAM FINDS IT
-
-// **INCLUSIVE random function**
 
 // returns false if could not find word
 // otherwise returns the coordinates in the array
 // which are where the word lies within the array.
 // params: arrayN - rows, arrayM - num of cols. 
-const wordSearcherAlgorithm = (word, array, arrayN, arrayM) =>{ 
+const wordSearcherAlgorithm = (word, array, arrayN, arrayM) =>{   
 
-    let startRow = 0;  
-    let startCol = 0;
+    console.log("serching for ", word); 
+    const backwardsWord = word.split("").reverse().join("");
     let coordinatePairs = []; 
     for (let i = 0; i < arrayN; i++){
         for (let j = 0; j < arrayM; j++){ 
             // checking forward words 
             let idx = 0;
-            startRow = i; 
-            startCol = j; 
-            while( word[idx].toLowerCase() == array[i][j + idx].toLowerCase() ){  
-                coordinatePairs.push( [i, j+idx++]); // increment idx  
-                if (idx == word.length){
-                    endRow = i; 
-                    endCol = j + idx -1;  	
+            while( word[idx].toLowerCase() === array[i][j + idx].toLowerCase() ){
+
+                coordinatePairs.push( [i, j+idx]); // increment idx
+                idx++; 
+                
+                if (idx >= word.length){
                     return coordinatePairs; // we found it 
                 }
                 if ( (j+idx >= arrayM ) ){ 
@@ -30,10 +25,124 @@ const wordSearcherAlgorithm = (word, array, arrayN, arrayM) =>{
                     break; // exit out loop 
                 }
             }
-            coordinatePairs = []; // reset array  
+            coordinatePairs = []; // reset array before next type of search 
+
+            idx = 0; // reset idx 
+            // checking backwards words 
+            // console.log('checking backwards word'); 
+            while( backwardsWord[idx].toLowerCase() === array[i][j + idx].toLowerCase() ){ 
+                
+                coordinatePairs.push( [i, j+idx]); // increment idx  
+                idx++; 
+                if (idx >= word.length){
+                    return coordinatePairs; // we found it  
+                }
+                if ( (j+idx >= arrayM ) ){ 
+
+                    break; // exit out loop 
+                }
+            }
+            
+            coordinatePairs = []; // reset array
+            idx = 0; 
+            // checking for straight line words - iterate through rows for each columns
+            idx = 0;
+            while(word[idx].toLowerCase() == array[i + idx][j].toLowerCase() ){
+                coordinatePairs.push( [i +idx, j]);
+                idx++;  // increment idx 
+                if (idx >= word.length){
+                    return coordinatePairs; 
+                }
+                if ( (i+ idx>= arrayN ) ){ // row error  //??
+                    break; // exit out the loop 
+                }
+            }
+            coordinatePairs = [];
+            // checking for straight line words that are backwards
+            // aka straigth line up words 
+            // - iterate through rows for each columns
+            idx = 0;
+            while(backwardsWord[idx].toLowerCase() == array[i + idx][j].toLowerCase() ){
+                coordinatePairs.push( [i + idx, j]);
+                idx++; 
+                if (idx >= word.length){
+                    console.log("found your word in straight line down backwards words");
+                    return coordinatePairs; 
+                }
+                if ( (i+ idx>= arrayN ) ){ // row error  //??
+                    break; // exit out the loop 
+                }
+            }
+            coordinatePairs = [];  
+            // checking diagonals which go from left to right
+            idx = 0; 
+            let row = i; 
+            let col = j; 
+            while( row < arrayN && col < arrayM){
+
+                if (word[idx].toLowerCase() === array[row][col].toLowerCase() ){
+                    coordinatePairs.push([row++, col++]); // increment after appending 
+                    idx++;
+                    if (idx >= word.length){ 
+                        return coordinatePairs; // found in diagonal  
+                    }
+                }else{
+                    break; 
+                }
+            }
+            coordinatePairs = [];
+            // checking diagonals left to right - reverse word 
+            idx = 0; 
+            row = i; 
+            col = j; 
+            while( row < arrayN && col < arrayM){
+
+                if (backwardsWord[idx] === array[row][col]){
+                    coordinatePairs.push([row++, col++]); // increment after appending 
+                    idx++;
+                    if (idx >= word.length){
+                        return coordinatePairs; // found in diagonal  
+                    }
+                }else{
+                    break; 
+                }
+            }
+            // checking for diagonals that go from right to left 
+            idx = 0; 
+            while( row < arrayN && arrayM >= 0 ){
+                if ( word[idx] === array[row][col]){
+                    coordinatePairs.push([row, col]); 
+                    row++; 
+                    col--; // we decrement the column 
+                    idx++;  
+                    if (idx >= word.length){
+                        return coordinatePairs; 
+                    }
+                }else{
+                    break; 
+                }
+            }
+            coordinatePairs = []; 
+            // checking for diagonals that go from right to left 
+            idx = 0; 
+            while( row < arrayN && arrayM >= 0 ){
+                if ( backwardsWord[idx] === array[row][col]){
+                    coordinatePairs.push([row, col]); 
+                    row++; 
+                    col--; // we decrement the column 
+                    idx++;  
+                    if (idx >= word.length){
+                        return coordinatePairs; 
+                    }
+                }else{
+                    break; 
+                }
+            }
+
+
         }
     }
-    return false; // did not find word 
+    return false; // did not find word  
     
 }
 
@@ -70,71 +179,100 @@ const createTable = (array) =>{
     });
     return table; 
 }
+// creates 2d array rep of table 
+// this lets us index in the same (row, col) manner as 
+// with the array where our word values are. 
+const createTableElementsArray = (table) =>{
+    let tableArray = []; 
+    const tableRows = table.querySelectorAll("tr");    
+    tableRows.forEach( (tableRow) =>{
+        let row = []; 
+        const tableCols = tableRow.querySelectorAll("td"); 
+        tableCols.forEach( (tableCol) =>{
+            row.push(tableCol); 
+        }); 
+        tableArray.push(row);   
+    }); 
+    return tableArray; 
+
+}
 
 window.onload = () =>{
     // dom elements 
     const wordSearcherInput = document.getElementById("word-to-search");
     const root = document.getElementById("word-search-table-container");
     const wordSearcherButton = document.getElementById("search-for-word"); 
-    let array = createArray(4, 5);
-    const wordSearcherTable = createTable(array);  
-    root.appendChild(wordSearcherTable);
-    const tableElements = wordSearcherTable.querySelectorAll("td");  
+    let array = createArray(4, 4);
+    const table = createTable(array); 
+    const wordSearcherTableArray = createTableElementsArray(table);  // an array of the table elements in table 
+    root.appendChild(table); 
 
-    wordSearcherButton.onclick = () =>{
+    wordSearcherButton.onclick = () =>{ 
         if (!wordSearcherInput.value){ 
-            return console.log('please return a valid value'); 
+            return wordSearcherInput.placeHolder = "Please return a value"; 
         }
-        const wordSearchValue = wordSearcherAlgorithm(wordSearcherInput.value, array, 4, 5); 
+        wordSearcherInput.placeHolder = "Enter word to search in table"; 
+
+        const wordSearchValue = wordSearcherAlgorithm(wordSearcherInput.value, array, array.length, array[0].length); 
         if (wordSearchValue){ 
             wordSearchValue.forEach( ([i, j])=>{
-                const rowLength = array[0].length; 
-                const index = (i*rowLength) + j; 
-                console.log("index", index); 
-                tableElements[index].style.color = 'red';  
+                console.log("i and j returned from alg:", i, j); 
+                wordSearcherTableArray[i][j].style.color = "red"; 
             } ); 
             return; 
         }
         console.log("could not find word: ", wordSearcherInput.value); 
     }
-    tableElements.forEach( (tableCol, index) =>{
-        // Reflects changes user makes in table
-        // to corresponding array we run our wordsearch
-        // algorithms on 
-        tableCol.oninput = ()=>{
-            const col = index % array[0].length;
-            const row = Math.floor( index/ array[0].length ); 
-             array[row][col] = tableCol.innerText;  
-             console.log(array[row][col]); 
-             console.log(array);   
+    // adding event listeners to table
+    // this variable fixes the cursor positioning bug 
+    // for when user goes up or left in texteditable content
+    // the cursor is actually appended to front and not to back
+    // so we track their positions in order to account for this 
+    let wentUpOrLeft = false;
+    for (let i = 0; i < wordSearcherTableArray.length; i++){
+        for (let j = 0; j < wordSearcherTableArray[i].length; j++){
+            // using add event listener here as we are adding 
+            // multiple events to the same element 
+            wordSearcherTableArray[i][j].addEventListener("input", () =>{
+                array[i][j] = wordSearcherTableArray[i][j].innerText;  
+            });
+            // for this event we want user to be able to arrows 
+            // to navigate through the table to allow for more 
+            // smooth user experience.  
+            wordSearcherTableArray[i][j].addEventListener("keydown", (event) =>{
+                if(event.key === "ArrowDown"){
+                    if (i +1 < wordSearcherTableArray.length ){
+                        wentUpOrLeft = false;
+                        wordSearcherTableArray[i+1][j].focus(); 
+                    }
+                }
+                if(event.key === "ArrowUp"){ 
+                    if (i -1 >= 0){
+                        wentUpOrLeft = true; 
+                        wordSearcherTableArray[i- 1][j].focus();
+                    }   
+                }
+                if(event.key === "ArrowLeft"){
+                    if (j - 1 >= 0){
+                        wentUpOrLeft = true; 
+                        wordSearcherTableArray[i][j -1].focus();
+                        
+                    }
+                }
+                if(event.key === "ArrowRight"){  
+                    if (j+1 < wordSearcherTableArray[i].length ){
+                        if (wentUpOrLeft){
+                            wentUpOrLeft = false;
+                            return;
+                        }
+                        wordSearcherTableArray[i][j + 1].focus();
+                        
+                    } 
+                }
+            });
         }
-        // user might be using keys to navigate down and up and left and right in table
-        // kind of tedious to just click and write into each textbox in table....   
-        tableCol.onkeydown = (event)=>{
-            if(event.key === "ArrowDown"){
-                if (index + array.length +1 < tableElements.length ){ 
-                    tableElements[index + array.length + 1 ].focus();
-                }
-            }
-            if(event.key === "ArrowUp"){ 
-                if (index - array.length -1 >= 0){ 
-                    tableElements[index - array.length -1].focus();
-                }   
-            }
-            if(event.key === "ArrowLeft"){
-                if (index -1 >= 0){
-                    tableElements[index -1].focus(); 
-                }
-            }
-            if(event.key === "ArrowRight"){ 
-                if (index +1 < tableElements.length){
-                    tableElements[index + 1].focus();  
-                } 
-            }
+    }
 
-       }
-    }); 
-    
 
     
 }
