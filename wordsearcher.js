744fe59d9c732728b58996b91c9824619c2f05ad
derikvanschaik <derikvanschaik@gmail.com>
2021-09-03 18:47:10 -0700

    refactored wordsearcher algorithm -- now recursive. lot more readable!

--- wordsearcher.js
+++ wordsearcher.js
@@ -1,150 +1,145 @@
 
+// recursive function used in wordsearcheralgorithm 
+const horizSearch = (i, j, idx, array, arrayM, word, matched) =>{ 
+    // base case 1 
+    if (j+idx >= arrayM || idx >= word.length){
+        const foundWord = idx >= word.length; 
+        return foundWord;  
+    }
+    // base case 2 
+    if (word[idx].toLowerCase() !== array[i][j+idx].toLowerCase() ){
+        return false; 
+    }
+    // else keep iterating 
+    matched.push( [i, j+idx]);  
+    idx++;
+    return horizSearch(i, j, idx, array, arrayM, word, matched);     
+}
+
+// recursive function used in wordsearcheralgorithm -- note changes like arrayN (row length ) from arrayM  (col length)
+const vertSearch = (i, j, idx, array, arrayN, word, matched) =>{
+        // base case 1 
+        if (i+idx >= arrayN || idx >= word.length){
+            const foundWord = idx >= word.length; 
+            return foundWord;  
+        }
+        // base case 2 
+        if (word[idx].toLowerCase() !== array[i +idx][j].toLowerCase() ){
+            return false; 
+        }
+        // else keep iterating 
+        matched.push( [i +idx, j]);  
+        idx++;
+        return vertSearch(i, j, idx, array, arrayN, word, matched);     
+
+}
+// recursive function used in wordsearcheralgorithm 
+const leftToRightDiagSearch = (row, col, idx, array, arrayN, arrayM, word, matched) =>{ 
+    // base case 1 
+    if (row>= arrayN || col>=arrayM || idx >= word.length){ 
+        const foundWord = idx >= word.length; 
+        return foundWord;  
+    }
+    // base case 2 
+    if (word[idx].toLowerCase() !== array[row][col].toLowerCase() ){ 
+        return false; 
+    }
+    // else keep iterating 
+    matched.push( [row++, col++]);   // increment row and cols 
+    idx++;
+    return leftToRightDiagSearch(row, col, idx, array, arrayN, arrayM, word, matched);      
+
+}
+// recursive function used in wordsearcheralgorithm 
+const rightToLeftDiagSearch = (row, col, idx, array, arrayN, arrayM, word, matched) =>{  
+    // base case 1 
+    if (row >= arrayN || col < 0 || idx >= word.length){ 
+        const foundWord = idx >= word.length; 
+        return foundWord;  
+    }
+    // base case 2 
+    if (word[idx].toLowerCase() !== array[row][col].toLowerCase() ){ 
+        return false; 
+    }
+    // else keep iterating 
+    matched.push( [row++, col--]);   // increment row but decrement col  
+    idx++;
+    return rightToLeftDiagSearch(row, col, idx, array, arrayN, arrayM, word, matched);      
+
+}
+
 // returns false if could not find word
 // otherwise returns the coordinates in the array
 // which are where the word lies within the array.
 // params: arrayN - rows, arrayM - num of cols. 
 const wordSearcherAlgorithm = (word, array, arrayN, arrayM) =>{   
 
-    console.log("serching for ", word); 
     const backwardsWord = word.split("").reverse().join("");
-    let coordinatePairs = []; 
+
     for (let i = 0; i < arrayN; i++){
         for (let j = 0; j < arrayM; j++){ 
-            // checking forward words 
-            let idx = 0;
-            while( word[idx].toLowerCase() === array[i][j + idx].toLowerCase() ){
 
-                coordinatePairs.push( [i, j+idx]); // increment idx
-                idx++; 
-                
-                if (idx >= word.length){
-                    return coordinatePairs; // we found it 
-                }
-                if ( (j+idx >= arrayM ) ){ 
-
-                    break; // exit out loop 
-                }
+            let matched = []; 
+            let row; 
+            let col; 
+            // searching for horizontal words 
+            let idx = 0; 
+            if (horizSearch(i, j, idx, array, arrayM, word, matched) ){
+                return matched; 
             }
-            coordinatePairs = []; // reset array before next type of search 
-
             idx = 0; // reset idx 
-            // checking backwards words 
-            // console.log('checking backwards word'); 
-            while( backwardsWord[idx].toLowerCase() === array[i][j + idx].toLowerCase() ){ 
-                
-                coordinatePairs.push( [i, j+idx]); // increment idx  
-                idx++; 
-                if (idx >= word.length){
-                    return coordinatePairs; // we found it  
-                }
-                if ( (j+idx >= arrayM ) ){ 
-
-                    break; // exit out loop 
-                }
+            matched = []; // reset array before next type of search 
+            if ( horizSearch( i , j, idx, array, arrayM, backwardsWord, matched) ){ 
+                return matched; 
             }
-            
-            coordinatePairs = []; // reset array
+            // searching for vertical words 
             idx = 0; 
-            // checking for straight line words - iterate through rows for each columns
-            while(word[idx].toLowerCase() == array[i + idx][j].toLowerCase() ){
-                coordinatePairs.push( [i +idx, j]);
-                idx++;  // increment idx 
-                if (idx >= word.length){
-                    return coordinatePairs; 
-                }
-                if ( (i+ idx>= arrayN ) ){ // row error  //??
-                    break; // exit out the loop 
-                }
+            matched = [];
+            if( vertSearch( i, j,  idx, array, arrayN, word, matched) ){
+                return matched; 
             }
-            coordinatePairs = [];
-            // checking for straight line words that are backwards
-            // aka straigth line up words 
-            // - iterate through rows for each columns
-            idx = 0;
-            while(backwardsWord[idx].toLowerCase() == array[i + idx][j].toLowerCase() ){
-                coordinatePairs.push( [i + idx, j]);
-                idx++; 
-                if (idx >= word.length){
-                    console.log("found your word in straight line down backwards words");
-                    return coordinatePairs; 
-                }
-                if ( (i+ idx>= arrayN ) ){ // row error  //??
-                    break; // exit out the loop 
-                }
-            }
-            coordinatePairs = [];  
-            // checking diagonals which go from left to right
             idx = 0; 
-            let row = i; 
-            let col = j; 
-            while( row < arrayN && col < arrayM){
-
-                if (word[idx].toLowerCase() === array[row][col].toLowerCase() ){
-                    coordinatePairs.push([row++, col++]); // increment after appending 
-                    idx++;
-                    if (idx >= word.length){ 
-                        return coordinatePairs; // found in diagonal  
-                    }
-                }else{
-                    break; 
-                }
+            matched = [];
+            if( vertSearch( i, j,  idx, array, arrayN, backwardsWord, matched) ){
+                return matched; 
             }
-            coordinatePairs = [];
-            // checking diagonals left to right - reverse word 
-            idx = 0; 
-            row = i; 
-            col = j; 
-            while( row < arrayN && col < arrayM){
+            // search for left to right diagonal words 
 
-                if (backwardsWord[idx].toLowerCase() === array[row][col].toLowerCase()){
-                    coordinatePairs.push([row++, col++]); // increment after appending 
-                    idx++;
-                    if (idx >= word.length){
-                        return coordinatePairs; // found in diagonal  
-                    }
-                }else{
-                    break; 
-                }
+            idx = 0; // reset idx
+            matched = [];
+            // init row and col vars
+            row = i;
+            col = j;
+            if (leftToRightDiagSearch(row, col, idx, array, arrayN, arrayM, word, matched) ){
+                return matched; 
             }
-            coordinatePairs = [];
-            // checking for diagonals that go from right to left 
-            idx = 0;
-            row = i; 
-            col = j; 
-            while( row < arrayN && col >= 0 ){
-                if ( word[idx].toLowerCase() === array[row][col].toLowerCase()){  
-                    coordinatePairs.push([row, col]); 
-                    row++; 
-                    col--; // we decrement the column 
-                    idx++;  
-                    if (idx >= word.length){
-                        return coordinatePairs; 
-                    }
-                }else{
-                    break; 
-                }
+            idx = 0; // reset idx
+            matched = [];
+            // reset row and col vars
+            row = i;
+            col = j;
+            if (leftToRightDiagSearch(row, col, idx, array, arrayN, arrayM, backwardsWord, matched) ){  
+                return matched; 
             }
-            coordinatePairs = []; 
-            // checking for diagonals that go from right to left 
-            idx = 0;
-            row = i; 
-            col = j; 
-            while( row < arrayN && col >= 0 ){
-                if ( backwardsWord[idx].toLowerCase() === array[row][col].toLowerCase()){ 
-                    coordinatePairs.push([row, col]); 
-                    row++; 
-                    col--; // we decrement the column 
-                    idx++;  
-                    if (idx >= word.length){
-                        return coordinatePairs; 
-                    }
-                }else{
-                    break; 
-                }
+            // searching for right to left diagonal words 
+            idx = 0; // reset idx
+            matched = [];
+            // reset row and col vars
+            row = i;
+            col = j;
+            if ( rightToLeftDiagSearch(row, col, idx, array, arrayN, arrayM, word, matched) ){
+                return matched; 
             }
-
-
-        }
+            idx = 0; // reset idx
+            matched = [];
+            // reset row and col vars
+            row = i;
+            col = j;
+            // final search for reverse words in right to left diagonal
+            if (rightToLeftDiagSearch(row, col, idx, array, arrayN, arrayM, backwardsWord, matched) ){
+                return matched; 
+            }            
+        } 
     }
     return false; // did not find word  
     
@@ -314,7 +309,8 @@ window.onload = () =>{
         // reset the place holder 
         wordSearcherInput.placeHolder = "Enter word to search in table"; 
         // run word search algorithm 
-        const wordSearchValue = wordSearcherAlgorithm(searchWord, array, array.length, array[0].length); 
+        const wordSearchValue = wordSearcherAlgorithm(searchWord, array, array.length, array[0].length);
+        console.log("word searcher value returned: ", wordSearchValue); 
         if (wordSearchValue){ 
             wordSearchValue.forEach( ([i, j])=>{
                 console.log("i and j returned from alg:", i, j); 
